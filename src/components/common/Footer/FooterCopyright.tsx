ded6eae0ed238a0728cd39974eeadf67c5be15de
Rai-prabhatraj <prabhatrajrai4@gmail.com>
2025-01-17 14:31:04 +0530

    chnage

--- src/components/common/Footer/FooterCopyright.tsx
+++ src/components/common/Footer/FooterCopyright.tsx
@@ -1,5 +1,4 @@
-import { Link } from "@tanstack/react-router";
-
+import React from "react";
 interface Props extends React.ComponentProps<"div"> {}
 
 export const FooterCopyright = ({ ...props }: Props) => {
